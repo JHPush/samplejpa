389ec39b26ea2d2d905deaee5d437fbc81432144
JHPush <qnrntmvls@gmail.com>
2025-02-10 18:31:41 +0900

    제출용- DB 정보 수정 (테이블이름, 유저)

--- src/main/java/com/example/backend_exam/repository/PostRepository.java
+++ src/main/java/com/example/backend_exam/repository/PostRepository.java
@@ -1,21 +1,13 @@
 package com.example.backend_exam.repository;
 
-import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.backend_exam.domain.Post;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-
-    @Query("SELECT COUNT(p) FROM Post As p")
-    long getTotalCount();
-
-    @Query("SELECT p FROM Post p")
-    List<Post> findAlls();
 
 
 }
